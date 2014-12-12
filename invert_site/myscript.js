1fd3b368d486baea3871e77a6c23715c1f73ab63
jshin04 <jshin04@risd.edu>
2014-12-11 23:17:52 -0500

    Whole buncha shit
    wholebuncha shit

--- invert_site/myscript.js
+++ invert_site/myscript.js
@@ -10,16 +10,4 @@ addEventListener("keydown", function(event) {
 
   });
 
-WebFontConfig = {
-    google: { families: [ 'Roboto+Condensed::latin', 'Share+Tech+Mono::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-
 $(document).ready(main);
